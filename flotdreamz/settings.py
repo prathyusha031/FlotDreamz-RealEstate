29c4b852582e7df2b8966bf9cff4663e00551f32
prathyusha031 <bailapudiprathyusha@gmail.com>
2026-06-19 12:09:21 +0530

    Fix ALLOWED_HOSTS for Render

--- flotdreamz/settings.py
+++ flotdreamz/settings.py
@@ -26,7 +26,11 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "flotdreamz.onrender.com",
+    "localhost",
+    "127.0.0.1",
+]
 
 
 # Application definition
