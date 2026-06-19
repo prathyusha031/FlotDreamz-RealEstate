aea65b3e48f62c66bc31dc460d6c73755057eac5
prathyusha031 <bailapudiprathyusha@gmail.com>
2026-06-19 11:26:44 +0530

    Render deployment configuration

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn flotdreamz.wsgi
--- flotdreamz/settings.py
+++ flotdreamz/settings.py
@@ -141,12 +141,15 @@ USE_TZ = True
 import os
 
 STATIC_URL = '/static/'
+
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 
-MEDIA_URL ='/media/'   
-MEDIA_ROOT = os.path.join(BASE_DIR,'media')  
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')  
 
 
 # Default primary key field type
@@ -159,5 +162,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'bailapudiprathyusha@gmail.com'
+EMAIL_HOST_PASSWORD = 'fvhk zytf egav kpta'
 DEFAULT_FROM_EMAIL = 'bailapudiprathyusha@gmail.com'   
 
